a2cbc0dc61a3bb6817bc15e333d129ce197e6300
ogochukwuugwu182608-rgb <ogochukwu.ugwu.182608@unn.edu.ng>
2025-12-10 06:51:20 -0800

    Update invoices.js

--- dashboard/js/pages/invoices.js
+++ dashboard/js/pages/invoices.js
@@ -13,12 +13,7 @@ export async function loadInvoicesPage() {
         
         console.log(currentInvoices)
 
-                         // const client_name =  invoice.client_name ? invoice.client_name : invoice.manual_client_name;
-                         //            const client_email =  invoice.client_email ? invoice.client_email : invoice.manual_client_email;
-                         //            const client_phone  =  invoice.client_phone ? invoice.client_phone : invoice.manual_client_phone;
-                         //            const quote_service =  invoice.quote_service ? invoice.quote_service : invoice.manual_service_name;
-                         //            const quote_reference  =  invoice.quote_reference ? invoice.quote_reference : N/A;
-
+                        
         
         pageContent.innerHTML = `
             <div class="card">
@@ -48,19 +43,26 @@ export async function loadInvoicesPage() {
                                 </tr>
                             </thead>
                             <tbody>
-                                ${currentInvoices.map(invoice => `
+                                ${currentInvoices.map(invoice => 
 
+                                     const client_name =  invoice.client_name ? invoice.client_name : invoice.manual_client_name;
+                                    const client_email =  invoice.client_email ? invoice.client_email : invoice.manual_client_email;
+                                    const client_phone  =  invoice.client_phone ? invoice.client_phone : invoice.manual_client_phone;
+                                    const quote_service =  invoice.quote_service ? invoice.quote_service : invoice.manual_service_name;
+                                    const quote_reference  =  invoice.quote_reference ? invoice.quote_reference : N/A;
 
                                    
                                         console.log(client_name);
+                                                      
+                                        return `
                                     <tr>
                                         <td><strong>${invoice.invoice_number}</strong></td>
                                         <td><span class="badge">${invoice.quote_reference}</span></td>
                                         <td>
                                             <div>
-                                                <strong>${ invoice.client_name ? invoice.client_name : invoice.manual_client_name;}</strong>
+                                                <strong>${client_name}</strong>
                                                 <br>
-                                                <small style="color: var(--text-secondary)">${ invoice.client_email ? invoice.client_email : invoice.manual_client_email}</small>
+                                                <small style="color: var(--text-secondary)">${ client_email }</small>
                                             </div>
                                         </td>
                                         <td>${invoice.quote_service || 'N/A'}</td>
